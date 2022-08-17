358ad0c5f85bcdbef75d7e22d3b2dcb8c6e66450
Khoshfekr MH <khoshfekr2001@protonmail.com>
2022-08-17 17:56:43 +0430

    now when localhost:3000 open a home page will come that connected to a footer and header

--- app.js
+++ app.js
@@ -15,11 +15,13 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+//#region Home page("/")
+app.get("/", function (req,res) {
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
 
-
-
-
-
+});
 
 
 
